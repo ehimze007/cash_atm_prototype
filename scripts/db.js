c00cd14e88fa1a34483fc87d3cc030f1fd2397a6
ehimze0007 <ehimze007@gmail.com>
2020-05-17 09:58:05 +0100

    Revert "Validate Card Number"
    This reverts commit cfa457b5a16203b65db0cddb50d216d74993f61b.

--- scripts/db.js
+++ scripts/db.js
@@ -10,37 +10,8 @@ const deepFreezeObject = (obj) => {
   return obj;
 };
 
-const LuhnAlgorithm = (digit) => {
-  if (/[^0-9\s]+/.test(digit)) return false;
-
-  digit = digit.replace(/\s/g, "");
-
-  if (digit.length <= 1) return false;
-
-  let sum = 0;
-  let secondDigit = false;
-
-  for (let i = digit.length - 1; i >= 0; i--) {
-    let odigit = digit.charAt(i);
-    let cdigit = parseInt(odigit);
-
-    if (secondDigit) {
-      cdigit *= 2;
-      if (cdigit > 9) {
-        cdigit = parseInt(cdigit / 10) + (cdigit % 10);
-      }
-    }
-
-    sum += cdigit;
-
-    secondDigit = !secondDigit;
-  }
-
-  return sum % 10 === 0;
-};
-
 const fakeDb = deepFreezeObject({
-  5500875451285166: {
+  543256789027635: {
     name: "Blessing Ehimze",
     pin: "8097",
     accBalance: 67679080,
@@ -48,7 +19,6 @@ const fakeDb = deepFreezeObject({
     blocked: false,
   },
 });
-
 
 const dataBaseCall = (ms) =>
   new Promise((resolve) => setTimeout(resolve(fakeDb), ms));
--- scripts/key.js
+++ scripts/key.js
@@ -1,22 +1,18 @@
 const keyPads = document.querySelector(".numeric_keypads");
+const displayScreen = document.querySelector(".atm_screen");
 const delete_button = document.querySelector(".delete_button");
-const cancel_button = document.querySelector(".cancel_button");
 const button_reponsiveness = document.querySelectorAll(".button_reponsiveness");
-const pin_value = document.querySelector('#pin');
 
 keyPads.addEventListener("click", (e) => {
   e.target.tagName === "BUTTON" &&
-   (pin_value.value += e.target.innerText);
-    
+    (displayScreen.innerHTML += e.target.textContent);
 });
 
 delete_button.addEventListener("click", () => {
-  const displayDigits = [...pin_value.value];
+  const displayDigits = [...displayScreen.innerHTML];
   displayDigits.pop();
-  pin_value.value = displayDigits.join("");
+  displayScreen.innerHTML = displayDigits.join("");
 });
-
-cancel_button.addEventListener("click", () => pin_value.value = "");
 
 // listen for clicks on the numeric_buttons and appropriate a style to the clicked button
 for (let i = 0; i < button_reponsiveness.length; i++) {
